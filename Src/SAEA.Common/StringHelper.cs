adff841e31d6b58eba7a6ea6aa6833a4f06de42b
wenli <wenguoli_520@qq.com>
2018-11-22 15:06:21 +0800

    v3.3.3.1

--- Src/SAEA.Common/StringHelper.cs
+++ Src/SAEA.Common/StringHelper.cs
@@ -5,7 +5,7 @@
 *公司名称：wenli
 *命名空间：SAEA.Commom
 *文件名： Class1
-*版本号： V3.3.1.2
+*版本号： V3.3.3.1
 *唯一标识：ef84e44b-6fa2-432e-90a2-003ebd059303
 *当前的用户域：WENLI-PC
 *创建人： yswenli
@@ -17,7 +17,7 @@
 *修改标记
 *修改时间：2018/3/1 15:54:21
 *修改人： yswenli
-*版本号： V3.3.1.2
+*版本号： V3.3.3.1
 *描述：
 *
 *****************************************************************************/
@@ -144,6 +144,11 @@ namespace SAEA.Common
         public static string Substring(this string str, int start)
         {
             return str.AsSpan(start, str.Length - start).ToString();
+        }
+
+        public static int ParseToInt(this string str,int start, int count)
+        {
+            return int.Parse(Substring(str, start, count));
         }
     }
 }
