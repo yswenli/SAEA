863308b60423733f7694aed3ec29f4b05ebc534b
yswenli <yswenli@outlook.com>
2020-01-08 16:20:15 +0800

    Update ConstHelper.cs

--- Src/SAEA.Common/ConstHelper.cs
+++ Src/SAEA.Common/ConstHelper.cs
@@ -63,7 +63,7 @@ namespace SAEA.Common
 
         public const string ONACTIONEXECUTED = "OnActionExecuted";
 
-        public const string SERVERMVCSERVER = "83,65,69,65,46,72,116,116,112,46,83,101,114,118,101,114,32,53,46,48";
+        public const string SERVERMVCSERVER = "83,101,114,118,101,114,58,32,83,65,69,65,46,72,116,116,112,46,83,101,114,118,101,114,32,53,46,48";
 
         public const string CT = "Content-Type";
 
