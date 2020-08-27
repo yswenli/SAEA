56be390a0314364bb429632a0f44ca3c7c3eb680
yswenli <yswenli@outlook.com>
2020-08-27 17:23:39 +0800

    Update SAEAVersion.cs

--- Src/SAEA.Common/SAEAVersion.cs
+++ Src/SAEA.Common/SAEAVersion.cs
@@ -29,7 +29,7 @@ namespace SAEA.Common
     /// </summary>
     public static class SAEAVersion
     {
-        const string version = "v5.3.3.2";
+        const string version = "v5.3.3.7";
 
         /// <summary>
         /// 获取版本
