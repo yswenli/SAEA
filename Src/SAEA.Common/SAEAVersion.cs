c23d886f9ee55c67ab1ae371de69e69495df22a5
yswenli <yswenli@outlook.com>
2021-04-17 19:23:42 +0800

    Update SAEAVersion.cs

--- Src/SAEA.Common/SAEAVersion.cs
+++ Src/SAEA.Common/SAEAVersion.cs
@@ -29,7 +29,7 @@ namespace SAEA.Common
     /// </summary>
     public static class SAEAVersion
     {
-        const string version = "v6.1.3.1";
+        const string version = "v6.2.6.1";
 
         /// <summary>
         /// 获取版本
