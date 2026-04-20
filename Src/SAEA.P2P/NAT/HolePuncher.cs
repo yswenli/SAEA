92bdd2057b104296ce6f3cd30926f37e7c32f447
WALLE <wenguoli_520@qq.com>
2026-04-20 16:07:31 +0800

    feat(p2p): add NAT detection and HolePuncher

---
+++ Src/SAEA.P2P/NAT/HolePuncher.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using SAEA.Sockets.Core.Udp;
+using SAEA.P2P.Protocol;
+using SAEA.P2P.Common;
+
+namespace SAEA.P2P.NAT
+{
+    public class HolePuncher
+    {
+        private P2PCoder _coder = new P2PCoder();
+        private HolePunchStrategy _strategy;
+        private int _timeout;
+        private int _maxRetry;
+        private NATDetector _detector;
+        
+        public event Action<IPEndPoint, byte[]> OnPunchPacketReceived;
+        public event Action<HolePunchResult> OnHolePunchCompleted;
+        
+        public HolePuncher(HolePunchStrategy strategy = HolePunchStrategy.PreferDirect, int timeout = 10000, int maxRetry = 3)
+        {
+            _strategy = strategy;
+            _timeout = timeout;
+            _maxRetry = maxRetry;
+            _detector = new NATDetector();
+        }
+        
+        public NATType GetNATType() => _detector.DetectedNATType;
+        public IPEndPoint GetPublicAddress() => _detector.PublicAddress;
+        public IPEndPoint GetLocalAddress() => _detector.LocalAddress;
+        
+        public void SetPublicAddress(IPEndPoint addr) => _detector.SetPublicAddress(addr);
+        public void SetNATType(NATType nat) => _detector.SetNATType(nat);
+        
+        public HolePunchResult CanPunchWith(NATType targetNat)
+        {
+            if (_strategy == HolePunchStrategy.RelayOnly)
+                return HolePunchResult.Failed("RelayOnly strategy", _detector.DetectedNATType, targetNat);
+            
+            if (_strategy == HolePunchStrategy.DirectOnly && !_detector.CanPunchWith(targetNat))
+                return HolePunchResult.Failed("NAT incompatible", _detector.DetectedNATType, targetNat);
+            
+            return null;
+        }
+        
+        public async Task<HolePunchResult> PunchAsync(UdpClientSocket socket, IPEndPoint targetAddr, string sessionId)
+        {
+            if (_strategy == HolePunchStrategy.RelayOnly)
+                return HolePunchResult.Failed("RelayOnly strategy", _detector.DetectedNATType, NATType.Unknown);
+            
+            var attempts = 0;
+            var startTime = DateTime.UtcNow;
+            
+            while (attempts < _maxRetry && (DateTime.UtcNow - startTime).TotalMilliseconds < _timeout)
+            {
+                var punchSync = _coder.EncodeP2P(P2PMessageType.PunchSync, 
+                    Encoding.UTF8.GetBytes(sessionId + "|" + DateTime.UtcNow.Ticks));
+                
+                socket.SendAsync(targetAddr, punchSync);
+                P2PLogHelper.Debug("HolePuncher", $"PunchSync sent to {targetAddr}, attempt {attempts + 1}");
+                
+                attempts++;
+                await Task.Delay(100);
+            }
+            
+            return HolePunchResult.Failed("Timeout waiting for response", _detector.DetectedNATType, NATType.Unknown);
+        }
+        
+        public void ProcessPunchSync(byte[] data, IPEndPoint source)
+        {
+            var protocols = _coder.DecodeP2P(data);
+            foreach (var p in protocols)
+            {
+                if (p.GetMessageType() == P2PMessageType.PunchSync)
+                {
+                    OnPunchPacketReceived?.Invoke(source, p.Content);
+                }
+            }
+        }
+    }
+}
--- Src/SAEA.P2P/Protocol/P2PMessageType.cs
+++ Src/SAEA.P2P/Protocol/P2PMessageType.cs
@@ -5,6 +5,8 @@ namespace SAEA.P2P.Protocol
         Register = 0x10,
         NatProbe = 0x20,
         PunchRequest = 0x30,
+        PunchSync = 0x31,
+        PunchAck = 0x32,
         RelayRequest = 0x40,
         LocalDiscover = 0x50,
         AuthChallenge = 0x60,
